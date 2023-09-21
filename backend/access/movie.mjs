bf8d50178ae0ef0d8a7baf489f4dced748f3a0ff
Trent <rexzhang68@gmail.com>
2023-09-21 16:37:49 -0500

    reformat sql query code

--- backend/access/movie.mjs
+++ backend/access/movie.mjs
@@ -2,77 +2,125 @@ import moment from "moment";
 import { executeSqlQuery } from "./sql/sql.mjs";
 
 export async function createCommentForMovie(email, movieId, comment, username) {
-  const statement =
-    "INSERT INTO movie_comments (user_email, movie_id, comment, created_time, username) VALUES (?, ?, ?, ?, ?);";
   const date = moment().format("YYYY-MM-DD HH:mm:ss");
-  await executeSqlQuery(statement, [email, movieId, comment, date, username]);
+  const statement = `
+    INSERT INTO movie_comments (user_email, movie_id, comment, created_time, username)
+    VALUES ('${email}', '${movieId}', '${comment}', '${date}', '${username}');
+  `;
+  await executeSqlQuery(statement);
 }
 
 export async function deleteLikeMovie(email, movie_id) {
-  const query =
-    "DELETE FROM user_liked_movie WHERE movie_id = ? and user_email = ?;";
-  await executeSqlQuery(query, [movie_id, email]);
+  const query = `
+    DELETE FROM user_liked_movie
+    WHERE movie_id = '${movie_id}' AND user_email = '${email}';
+  `;
+  await executeSqlQuery(query);
 }
 
 export async function getCommentsByMovieId(movieId) {
-  const query = "SELECT * FROM movie_comments WHERE movie_id = ?;";
-  return await executeSqlQuery(query, [movieId]);
+  const query = `
+    SELECT * FROM movie_comments
+    WHERE movie_id = '${movieId}';
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getMoviesSortedByRating(limit) {
-  const query = "SELECT * FROM movie ORDER BY rating LIMIT ?;";
-  return await executeSqlQuery(query, [limit]);
+  const query = `
+    SELECT * FROM movie
+    ORDER BY rating DESC
+    LIMIT ${limit};
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getMoviesRandom(limit) {
-  const query = "SELECT * FROM movie ORDER BY RAND() LIMIT ?;";
-  return await executeSqlQuery(query, [limit]);
+  const query = `
+    SELECT * FROM movie
+    ORDER BY RAND()
+    LIMIT ${limit};
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getMoviesByUserLiked(email) {
-  const query =
-    "select * from movie where id in (select movie_id from user_liked_movie where user_email = ?);";
-  return await executeSqlQuery(query, [email]);
+  const query = `
+    SELECT * FROM movie
+    WHERE id IN (
+      SELECT movie_id
+      FROM user_liked_movie
+      WHERE user_email = '${email}'
+    );
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getMoviesByListId(listId) {
-  const query =
-    "select * from movie where id in (select movie_id from list2movie where list_id = ?);";
-  return await executeSqlQuery(query, [listId]);
+  const query = `
+    SELECT * FROM movie
+    WHERE id IN (
+      SELECT movie_id
+      FROM list2movie
+      WHERE list_id = '${listId}'
+    );
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getMoviesByUserEmail(email) {
-  const query1 = "SELECT movie_id FROM user_liked_movie WHERE user_email = ?;";
-  const movieIdList = await executeSqlQuery(query1, [email]);
+  const query1 = `
+    SELECT movie_id
+    FROM user_liked_movie
+    WHERE user_email = '${email}';
+  `;
+  const movieIdList = await executeSqlQuery(query1);
 }
 
 export async function getMoviesById(id) {
-  const query = "SELECT * FROM movie WHERE id = ?;";
-  return await executeSqlQuery(query, [id]);
+  const query = `
+    SELECT * FROM movie
+    WHERE id = '${id}';
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getRecommendedMoviesSortedByRating(limit, email) {
-  const query =
-    "SELECT * FROM movie WHERE id not in (select movie_id from user_liked_movie where user_email = ?) ORDER BY rating LIMIT ?;";
-  return await executeSqlQuery(query, [email, limit]);
+  const query = `
+    SELECT * FROM movie
+    WHERE id NOT IN (
+      SELECT movie_id
+      FROM user_liked_movie
+      WHERE user_email = '${email}'
+    )
+    ORDER BY rating
+    LIMIT ${limit};
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function getUserLikedMovieByEmailAndMovieId(email, id) {
-  var query =
-    "select * from user_liked_movie where user_email = ? and movie_id = ?;";
-  return await executeSqlQuery(query, [email, id]);
+  const query = `
+    SELECT * FROM user_liked_movie
+    WHERE user_email = '${email}' AND movie_id = '${id}';
+  `;
+  return await executeSqlQuery(query);
 }
 
 export async function likeMovie(email, movie_id) {
-  const query =
-    "INSERT INTO user_liked_movie (movie_id, user_email) VALUES (?, ?);";
-  await executeSqlQuery(query, [movie_id, email]);
+  const query = `
+    INSERT INTO user_liked_movie (movie_id, user_email)
+    VALUES ('${movie_id}', '${email}');
+  `;
+  await executeSqlQuery(query);
 }
 
 export async function rateMoviesById(email, movieId, rating) {
-  const statement =
-    "INSERT INTO rating (user, movie_id, rating) VALUES (?, ?, ?);";
-  await executeSqlQuery(statement, [email, movieId, rating]);
+  const statement = `
+    INSERT INTO rating (user, movie_id, rating)
+    VALUES ('${email}', '${movieId}', '${rating}');
+  `;
+  await executeSqlQuery(statement);
 }
 
 export async function searchMovies(keyword, type, language) {
@@ -96,11 +144,19 @@ export async function searchMovies(keyword, type, language) {
 }
 
 export async function updateMovieRatingById(movieId, rating, num) {
-  const query = "UPDATE movie SET rating = ?, rate_num = ? WHERE id = ?;";
-  await executeSqlQuery(query, [rating, num, movieId]);
+  const query = `
+    UPDATE movie
+    SET rating = '${rating}', rate_num = '${num}'
+    WHERE id = '${movieId}';
+  `;
+  await executeSqlQuery(query);
 }
 
 export async function updateMovieLikedById(id, likedNum) {
-  const statement = "UPDATE movie SET liked_num = ? WHERE id = ?;";
-  await executeSqlQuery(statement, [likedNum, id]);
+  const statement = `
+    UPDATE movie
+    SET liked_num = '${likedNum}'
+    WHERE id = '${id}';
+  `;
+  await executeSqlQuery(statement);
 }
