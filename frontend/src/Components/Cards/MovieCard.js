f3e3b022478deae2a6d950a49560a9aca725b3f5
Trent <rexzhang68@gmail.com>
2023-09-20 23:55:46 -0500

    front end: fixed movie detail page

--- frontend/src/Components/Cards/MovieCard.js
+++ frontend/src/Components/Cards/MovieCard.js
@@ -4,31 +4,39 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ info }) => {
-  const login = JSON.parse(window.localStorage.getItem("login"));
+  return (
+    <Card className="movie_card">
+      <Card.Body>
+        <Link className="movie_card_link" to={`/movie/${info.id}`}>
+          <Card.Title>{info.title}</Card.Title>
+          <Card.Img src={coverURL(info.cover)} />
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+  //   const login = JSON.parse(window.localStorage.getItem("login"));
 
-  if (!login) {
-    // console.log("mc"+login);
-    return (
-      <Card className="movie_card">
-        <Card.Body>
-          <Link className="movie_card_link" to={"/home/"}>
-            <Card.Title>{info.title}</Card.Title>
-            <Card.Img src={coverURL(info.cover)} />
-          </Link>
-        </Card.Body>
-      </Card>
-    );
-  } else {
-    // console.log("mc" + login);
-    return (
-      <Card className="movie_card">
-        <Card.Body>
-          <Link className="movie_card_link" to={"/movie/".concat(info.id)}>
-            <Card.Title>{info.title}</Card.Title>
-            <Card.Img src={coverURL(info.cover)} />
-          </Link>
-        </Card.Body>
-      </Card>
-    );
-  }
+  //   if (!login) {
+  //     return (
+  //       <Card className="movie_card">
+  //         <Card.Body>
+  //           <Link className="movie_card_link" to={"/home/"}>
+  //             <Card.Title>{info.title}</Card.Title>
+  //             <Card.Img src={coverURL(info.cover)} />
+  //           </Link>
+  //         </Card.Body>
+  //       </Card>
+  //     );
+  //   } else {
+  //     return (
+  //       <Card className="movie_card">
+  //         <Card.Body>
+  //           <Link className="movie_card_link" to={"/movie/".concat(info.id)}>
+  //             <Card.Title>{info.title}</Card.Title>
+  //             <Card.Img src={coverURL(info.cover)} />
+  //           </Link>
+  //         </Card.Body>
+  //       </Card>
+  //     );
+  //   }
 };
