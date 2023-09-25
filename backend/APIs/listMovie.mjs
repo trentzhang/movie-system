a16a0669e54747581621ef5b3a7db235bd20e82e
Trent <rexzhang68@gmail.com>
2023-09-24 20:56:48 -0500

    fixed api: add movie to list

---
+++ backend/APIs/listMovie.mjs
@@ -0,0 +1,17 @@
+import { addMovieToList } from "../SQLQueries/list.mjs";
+
+export async function putMovieToListAPI(req, res) {
+  try {
+    const listId = req.params.list_id;
+    const movieId = req.params.movie_id;
+
+    await addMovieToList(listId, movieId);
+    return res.status(200).send({ message: "OK", data: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "ERROR: Encounter error during adding movie to list.",
+      data: err,
+    });
+  }
+}
