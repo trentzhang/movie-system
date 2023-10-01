412d4cb8872e5ce2b2230150cc4653e46c2dce9a
Trent <rexzhang68@gmail.com>
2023-10-01 17:28:38 -0500

    front end unlimited search scrolling

--- frontend/src/Search/Search.js
+++ frontend/src/Search/Search.js
@@ -5,89 +5,20 @@ import {
   Container,
   Form,
   FormControl,
-  Image,
   Row,
   Stack,
 } from "react-bootstrap";
 import { backendUrl } from "../settings";
-
-import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import { MovieCard } from "../Home/body/MovieCardGroup";
-
-// import { checkbox } from "./body/checkboxes";
-export function ResultCardMovie(item) {
-  const login = JSON.parse(window.localStorage.getItem("login"));
-
-  const handleNotLogin = () => {
-    if (!login) {
-      alert("Pleas Log In or Sign Up First!");
-    }
-  };
-  const handleAddToList = (mv_id) => {
-    const request = {
-      method: "PUT",
-      credentials: "same-origin",
-      headers: {
-        "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    };
-
-    fetch(`${backendUrl}/user/lists/movies/` + mv_id, request);
-  };
-
-  if (item.valueProps.cover === "none") {
-    item.valueProps.cover =
-      "//st.depositphotos.com/1987177/3470/v/450/depositphotos_34700099-stock-illustration-no-photo-available-or-missing.jpg";
-  }
-  return (
-    <Container className="mb-2 text-bg-dark">
-      <script src="holder.js"></script>
-
-      <Row>
-        <Col xs="2">
-          <Link className="movie_link" to={"/movie/" + item.valueProps.id}>
-            <Image
-              src={item.valueProps.cover}
-              height="150"
-              width="100"
-              className="mx-auto"
-            />
-          </Link>
-        </Col>
-        <Col xs="9">
-          <Link
-            className="movie_link"
-            to={login ? "/movie/" + item.valueProps.id : "/advanced_search/"}
-          >
-            <h3>{item.valueProps.title}</h3>
-          </Link>
-          <div>Runtime: {item.valueProps.runtime} min</div>
-          <div>Type: {item.valueProps.type}</div>
-        </Col>
-        <Col className="m-auto">
-          <Button
-            variant="outline-primary"
-            className="float_right"
-            onClick={() => {
-              handleNotLogin();
-              handleAddToList(item.valueProps.id);
-            }}
-          >
-            +
-          </Button>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
 
 const Search = () => {
   const [keyword, setKeyword] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [language, setLanguage] = useState("");
   const [type, setType] = useState("");
+  const [page, setPage] = useState(0);
+
   const lan_list = [
     "English",
     "Spanish",
@@ -107,76 +38,92 @@ const Search = () => {
     "Sci_Fi",
   ];
 
-  const [searchJSON, setSearchJSON] = useState("");
-
-  const handleSearch = useCallback(() => {
-    const request = {
-      method: "POST",
-      credentials: "same-origin",
-      headers: {
-        "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: window.localStorage.getItem("searchJSON"),
-    };
-
-    fetch(`${backendUrl}/search_movie`, request)
-      .then((response) => {
-        return response.json();
-      })
-      .then(
-        (response) => {
-          if (!response.data) {
-            console.log("no results");
-            setSearchResults(["No results!"]);
-          } else {
-            setSearchResults(response.data);
-          }
-        },
-        (ex) => {}
-      );
-  }, []);
-
-  useEffect(() => {
-    window.localStorage.setItem("searchJSON", searchJSON);
-  }, [searchJSON]);
-
-  const updateSearchJSON = useCallback(() => {
-    setSearchJSON(
-      JSON.stringify({
+  const handleSearch = useCallback(async () => {
+    try {
+      setPage(0);
+      const searchCriteria = {
         language: language,
         type: type,
         keyword: keyword,
         searchType: "Movie",
-      })
-    );
+        page: 0,
+      };
+
+      const request = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(searchCriteria),
+      };
+      const response = await fetch(`${backendUrl}/search_movie`, request);
+      const data = await response.json();
+
+      if (!data.data) {
+        console.log("no results");
+        setSearchResults(["No results!"]);
+      } else {
+        setSearchResults(data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
   }, [keyword, language, type]);
 
-  useEffect(() => {
-    updateSearchJSON();
-  }, [language, type, keyword, updateSearchJSON]);
-
   const handleChangeLanguage = (languageName) => {
-    if (language === languageName) {
-      setLanguage("");
-    } else {
-      setLanguage(languageName);
-    }
-    updateSearchJSON();
+    setLanguage((prevLanguage) =>
+      prevLanguage === languageName ? "" : languageName
+    );
   };
 
   const handleChangeType = (typeName) => {
-    if (type === typeName) {
-      setType("");
-    } else {
-      setType(typeName);
-    }
-    updateSearchJSON();
+    setType((prevType) => (prevType === typeName ? "" : typeName));
   };
-  const [languages, setLanguages] = useState([]);
+
+  // Attach the scroll event listener
   useEffect(() => {
-    console.log("languages :>> ", languages);
-  }, [languages]);
+    const handleScroll = async () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        try {
+          const searchCriteria = {
+            language: language,
+            type: type,
+            keyword: keyword,
+            searchType: "Movie",
+            page: page + 1,
+          };
+
+          const request = {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify(searchCriteria),
+          };
+          const response = await fetch(`${backendUrl}/search_movie`, request);
+          const data = await response.json();
+
+          setPage((prevPage) => prevPage + 1);
+          setSearchResults((prevResults) => [
+            ...prevResults,
+            ...(data.data || []),
+          ]);
+        } catch (error) {
+          console.error("Error fetching additional search results:", error);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [page]);
 
   return (
     <Stack gap={3}>
@@ -193,11 +140,7 @@ const Search = () => {
               type="search"
               placeholder="Search"
               className="me-2"
-              onChange={(e) => {
-                e.preventDefault();
-                console.log(e.target.value);
-                setKeyword(e.target.value);
-              }}
+              onChange={(e) => setKeyword(e.target.value)}
               value={keyword}
             />
             <Button variant="light" onClick={handleSearch}>
@@ -207,75 +150,43 @@ const Search = () => {
           <Stack key="search-box-stack" className="mb-3">
             <div>
               Language:
-              {/* TODO allow multiple genre or language selection */}
               <Container>
-                {Array.from(lan_list).map((l) => {
-                  return (
-                    <Form.Check
-                      label={l}
-                      inline
-                      type="checkbox"
-                      checked={language === l}
-                      onChange={() => handleChangeLanguage(l)}
-                    />
-                  );
-                })}
-              </Container>
-              <Container>
-                <Form>
-                  {Array.from(lan_list).map((l) => {
-                    return (
-                      <Form.Check
-                        onInput={(item) => {
-                          console.log("item :>> ", item);
-                        }}
-                        label={l}
-                        inline
-                        type="checkbox"
-                        //   checked={language === l}
-                        onChange={() => {
-                          if (languages.includes(l)) {
-                            setLanguages(
-                              languages.filter((item) => item !== l)
-                            );
-                          } else {
-                            setLanguages([...languages, l]);
-                          }
-                          console.log("l :>> ", l);
-                        }}
-                      />
-                    );
-                  })}
-                </Form>
+                {lan_list.map((l) => (
+                  <Form.Check
+                    key={l}
+                    label={l}
+                    inline
+                    type="checkbox"
+                    checked={language === l}
+                    onChange={() => handleChangeLanguage(l)}
+                  />
+                ))}
               </Container>
             </div>
             <div>
               Genre:
               <Container>
-                {Array.from(type_list).map((t) => {
-                  return (
-                    <Form.Check
-                      label={t}
-                      inline
-                      type="checkbox"
-                      checked={type === t}
-                      onChange={() => handleChangeType(t)}
-                    />
-                  );
-                })}
+                {type_list.map((t) => (
+                  <Form.Check
+                    key={t}
+                    label={t}
+                    inline
+                    type="checkbox"
+                    checked={type === t}
+                    onChange={() => handleChangeType(t)}
+                  />
+                ))}
               </Container>
             </div>
           </Stack>
           {/* Results Card */}
           <Stack className="text-bg-dark" gap={3}>
             <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
-              {searchResults.map((item, index) => {
-                return (
-                  <Col>
-                    <MovieCard movieInformation={item}></MovieCard>
-                  </Col>
-                );
-              })}
+              {searchResults.map((item, index) => (
+                <Col key={index}>
+                  <MovieCard movieInformation={item}></MovieCard>
+                </Col>
+              ))}
             </Row>
           </Stack>
         </Stack>
